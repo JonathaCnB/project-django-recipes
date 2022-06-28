6825aec9d477731663836838817939eb0a33c4ff
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-06-28 16:17:29 -0400

    chore: files core project

---
+++ project/settings.py
@@ -0,0 +1,128 @@
+from pathlib import Path
+
+from decouple import config
+from django.contrib.messages import constants
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = config("SECRET_KEY")
+
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
+
+INSTALLED_APPS = [
+    # apps django
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    # local apps
+    "recipes",
+    "users.apps.UsersConfig",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "project.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "project.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  # noqa
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  # noqa
+    },
+]
+
+# User Model
+
+AUTH_USER_MODEL = "users.User"
+
+SITE_ID = 1
+
+LANGUAGE_CODE = "pt-BR"
+
+TIME_ZONE = "America/Manaus"
+
+USE_I18N = True
+
+USE_TZ = True
+
+DATE_FORMAT = "d-m-Y"
+
+DATETIME_FORMAT = "d, M, Y - P"
+
+TIME_FORMAT = "G:i"
+
+USE_THOUSAND_SEPARATOR = True
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
+
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MESSAGE_TAGS = {
+    constants.DEBUG: "message-debug",
+    constants.ERROR: "message-error",
+    constants.INFO: "message-info",
+    constants.SUCCESS: "message-success",
+    constants.WARNING: "message-warning",
+}
+
+SELENIUM_HEADLESS = config("SELENIUM_HEADLESS", default=False, cast=bool)
+
+PER_PAGES = config("PER_PAGES", default=6, cast=int)
