2f8167001f8e3dd0f92713639b4ac39b25d9df00
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-06-29 22:54:52 -0400

    feat: create menu and Implemented  form logout

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -1,16 +1,50 @@
-function my_scope() {
+(() => {
     const forms = document.querySelectorAll('.form-delete');
-
     for (const form of forms) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             const confirmed = confirm('Você tem certeza?');
-
+    
             if (confirmed) {
                 form.submit();
             }
         })
-    }   
-};
+    }
 
-my_scope()
+})();
+
+
+(() => {
+    const buttonClosedMenu = document.querySelector('.button-close-menu');
+    const buttonShowMenu = document.querySelector('.button-show-menu');
+    const menuContainer = document.querySelector('.menu-container');
+
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+    const menuHiddenClass = 'menu-hidden';
+
+    const closeMenu = () => {
+        buttonShowMenu.classList.add(buttonShowMenuVisibleClass)
+        menuContainer.classList.add(menuHiddenClass)
+    }
+    
+    const showMenu = () => {
+        buttonShowMenu.classList.remove(buttonShowMenuVisibleClass)
+        menuContainer.classList.remove(menuHiddenClass)
+    }
+
+    buttonShowMenu?.addEventListener('click', () => showMenu())
+    buttonClosedMenu?.addEventListener('click', () => closeMenu())
+    
+})();
+
+(() => {
+ const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+ const formLogout = document.querySelector('.form-logout');
+
+ for (const link of authorsLogoutLinks) {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        formLogout.submit();
+    });
+ };
+})();
